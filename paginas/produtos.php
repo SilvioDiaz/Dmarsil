8facc7fee0e9cd6b690ccbc8b015c083fedbe0f2
SilvioDiaz <57494579+SilvioDiaz@users.noreply.github.com>
2021-01-04 19:21:38 -0300

    Organização Pagina Produto

--- paginas/produtos.php
+++ paginas/produtos.php
@@ -24,89 +24,22 @@ document.title = 'Produtos'
 
 ?>
 
-<div class="containerLista">
-    <div class="adminArea">
-        <div class ="catProduto">
-            <div class="tabela">
-                <h1>Categorias</h1>
-                <a class="link_botao" href="?pagina=cadastro_categoria">Cadastrar nova Categoria</a>
-                <table cellspacing=0>
-                    <tr>
-                        <th>DESCRIÇÃO</th>
-                        <th colspan="2">FUNÇÃO</th>
-                    </tr>
-
-                    <?php
-                        while($linha = mysqli_fetch_array($tipo)){
-
-                        ?>
-                            <tr>
-                            <td><?=$linha['nome_tipoProduto']?></td>
-                            <td class=tabela-celula><a href="?pagina=cadastro_categoria&id_categoria=<?=$linha['id_tipoProduto']?>">Editar</td>
-                            <td class=tabela-celula><a href="./script/excluir_categoria.php?id=<?=$linha['id_tipoProduto']?>">Deletar</a></td>
-                            </tr>
-                    <?php
-                        }
-                    ?>
-                </table>
-                
-            </div>
-
-            <div class="tabela">
-                <h1>Banho:</h1>
-                <a class="link_botao" href="?pagina=cadastro_banho">Cadastrar novo Banho</a>
-                <table cellspacing=0>
-                    <tr>
-                        <th>DESCRIÇÃO</th>
-                        <th colspan="2">FUNÇÃO</th>
-                    </tr>
-
-                    <?php
-                        while($linha = mysqli_fetch_array($banho)){
-
-                        ?>
-                            <tr>
-                            <td><?=$linha['nome_banho']?></td>
-                            <td class=tabela-celula><a href="?pagina=cadastro_banho&id_banho=<?=$linha['id_banho']?>">Editar</td>
-                            <td class=tabela-celula><a href="./script/excluir_banho.php?id=<?=$linha['id_banho']?>">Deletar</a></td>
-                            </tr>
-                    <?php
-                        }
-                    ?>
-                </table>
-                
-            </div>
-
-            <div class="tabela">
-                <h1>Modelo</h1>
-                <a class="link_botao" href="?pagina=cadastro_modelo">Cadastrar novo Modelo</a>
-                <table cellspacing=0>
-                    <tr>
-                        <th>DESCRIÇÃO</th>
-                        <th colspan="2">FUNÇÃO</th>
-                    </tr>
-
-                    <?php
-                        while($linha = mysqli_fetch_array($modelo)){
-
-                        ?>
-                            <tr>
-                            <td><?=$linha['nome_modelo']?></td>
-                            <td class=tabela-celula><a href="?pagina=cadastro_modelo&id_modelo=<?=$linha['id_modelo']?>">Editar</td>
-                            <td class=tabela-celula><a href="./script/excluir_modelo.php?id=<?=$linha['id_modelo']?>">Deletar</a></td>
-                            </tr>
-                    <?php
-                        }
-                    ?>
-                </table>
-                
-            </div>
-        </div>
+<div class="selecaoProduto">
+    <p>
+        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#produtos" aria-expanded="false" aria-controls="produtos">Produtos</button>
+        <a class="btn btn-primary" data-toggle="collapse" href="#categoria" role="button" aria-expanded="false" aria-controls="categoria">Categorias</a>
+        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#banho" aria-expanded="false" aria-controls="banho">Banho</button>
+        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#modelo" aria-expanded="false" aria-controls="modelo">Modelo</button>
+    </p>
+</div>
+<div class="row">
+    <!-- 
+  Produtos -->
+  <div class="col">
+    <div class="collapse multi-collapse" id="produtos">
 
 
-
-
-        <div class="tabela">
+    <div class="tabela">
             <h1>Produtos Cadastrados</h1>
             <a class="link_botao" href="?pagina=cadastro_produtos">Cadastrar novo Produto</a>
             <table cellspacing=0>
@@ -133,5 +66,107 @@ document.title = 'Produtos'
             </table>
         
         </div>
+
     </div>
+  </div>
+
+  <div class="col">
+    <div class="collapse multi-collapse" id="categoria">
+     
+    <!-- CATEGORIA -->
+      <div class="tabela">
+                <h1>Categorias</h1>
+                <a class="link_botao" href="?pagina=cadastro_categoria">Cadastrar nova Categoria</a>
+                <table cellspacing=0>
+                    <tr>
+                        <th>DESCRIÇÃO</th>
+                        <th colspan="2">FUNÇÃO</th>
+                    </tr>
+
+                    <?php
+                        while($linha = mysqli_fetch_array($tipo)){
+
+                        ?>
+                            <tr>
+                            <td><?=$linha['nome_tipoProduto']?></td>
+                            <td class=tabela-celula><a href="?pagina=cadastro_categoria&id_categoria=<?=$linha['id_tipoProduto']?>">Editar</td>
+                            <td class=tabela-celula><a href="./script/excluir_categoria.php?id=<?=$linha['id_tipoProduto']?>">Deletar</a></td>
+                            </tr>
+                    <?php
+                        }
+                    ?>
+                </table>
+                
+            </div>
+
+    </div>
+  </div>
+
+    <!-- BANHO -->
+  <div class="col">
+    <div class="collapse multi-collapse" id="banho">
+
+
+      <div class="tabela">
+                <h1>Banho:</h1>
+                <a class="link_botao" href="?pagina=cadastro_banho">Cadastrar novo Banho</a>
+                <table cellspacing=0>
+                    <tr>
+                        <th>DESCRIÇÃO</th>
+                        <th colspan="2">FUNÇÃO</th>
+                    </tr>
+
+                    <?php
+                        while($linha = mysqli_fetch_array($banho)){
+
+                        ?>
+                            <tr>
+                            <td><?=$linha['nome_banho']?></td>
+                            <td class=tabela-celula><a href="?pagina=cadastro_banho&id_banho=<?=$linha['id_banho']?>">Editar</td>
+                            <td class=tabela-celula><a href="./script/excluir_banho.php?id=<?=$linha['id_banho']?>">Deletar</a></td>
+                            </tr>
+                    <?php
+                        }
+                    ?>
+                </table>
+                
+            </div>
+
+    </div>
+  </div>
+
+  <!-- 
+    MODELO -->
+  <div class="col">
+    <div class="collapse multi-collapse" id="modelo">
+
+    <div class="tabela">
+                <h1>Modelo</h1>
+                <a class="link_botao" href="?pagina=cadastro_modelo">Cadastrar novo Modelo</a>
+                <table cellspacing=0>
+                    <tr>
+                        <th>DESCRIÇÃO</th>
+                        <th colspan="2">FUNÇÃO</th>
+                    </tr>
+
+                    <?php
+                        while($linha = mysqli_fetch_array($modelo)){
+
+                        ?>
+                            <tr>
+                            <td><?=$linha['nome_modelo']?></td>
+                            <td class=tabela-celula><a href="?pagina=cadastro_modelo&id_modelo=<?=$linha['id_modelo']?>">Editar</td>
+                            <td class=tabela-celula><a href="./script/excluir_modelo.php?id=<?=$linha['id_modelo']?>">Deletar</a></td>
+                            </tr>
+                    <?php
+                        }
+                    ?>
+                </table>
+                
+            </div>
+
+    </div>
+  </div>
+
+
 </div>
