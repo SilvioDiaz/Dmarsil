ba312039034e06daff28488a650c18c39b9d4cb2
SilvioDiaz <57494579+SilvioDiaz@users.noreply.github.com>
2021-01-08 18:43:06 -0300

    Perfil basico completo

--- paginas/perfil.php
+++ paginas/perfil.php
@@ -23,74 +23,91 @@ while($linha = mysqli_fetch_array($consulta)){
 ?>
 
 <div class="container">
+    <div class="row">
 
-    <div id="perfil_dados">
+        <div class="col-2">
+            <div id="perfil_menu">
             
-            <div class="row">
-                <div id="perfil">
-                    <h5>Nome:</h5> <p><?=$linha['nome']?></p>
-                </div>
+            <a href="?pagina=pedidos" class="btn_menuPerfil"> Pedidos      
+            </a>
+        </div>
 
-                <div id="perfil_Telefone">
-                    <h5>Telefone:</h5> <p><?=$linha['telefone']?></p>
-                </div>
-            </div>
+        </div>
 
-            <div class="row">    
-                <div id="perfil_email">
-                    <h5>E-mail:</h5> <p><?=$linha['email']?></p>
-                </div>
-
-                <div id="perfil_nascimento">
-                    <h5>Nascimento:</h5> <p><?=$linha['nascimento']?></p>
-                </div>
-
-            </div>
-
-            <div class="row">
-                <div id="perfil_Cep">
-                    <h5>Cep:</h5> <p><?=$linha['cep']?></p>
-                </div>
-            </div> 
-
-            <div class="row">
-            <div id="perfil_endereco">
-                <h5>Endereço:</h5> <p><?=$linha['endereco']?></p>
-            </div>
-            <div id="perfil_numero">
-                <h5>Numero:</h5> <p><?=$linha['numero']?></p>
-            </div>
-            </div>
-            <div class="row">
-                <div id="perfil_Complemento">
-                    <h5>Complemento:</h5> <p><?=$linha['complemento']?></p>
-                </div>
-            </div>
-
-            <div class="row">
-                <div id="perfil_Cidade">
-                    <h5>Cidade:</h5> <p><?=$linha['cidade']?></p>
-                </div>
-
-                <div id="perfil_Bairro">
-                    <h5>Bairro:</h5> <p><?=$linha['bairro']?></p>
-                </div>
+        <div id="perfil_dados" class="col-6">
                 
+                <div class="row">
+                    <div id="perfil" class="linha_perfil">
+                        <h5>Nome:</h5> <p><?=$linha['nome']?></p>
+                    </div>
+
+                    <div id="perfil_Telefone"  class="linha_perfil">
+                        <h5>Telefone:</h5> <p><?=$linha['telefone']?></p>
+                    </div>
+
+
+                    <div id="perfil_email"  class="linha_perfil">
+                        <h5>E-mail:</h5> <p><?=$linha['email']?></p>
+                    </div>
+
+                </div>
+
+                <div class="row">    
+
+                    <div id="perfil_nascimento"  class="linha_perfil">
+                        <h5>Nascimento:</h5> <p><?=$linha['nascimento']?></p>
+                    </div>
+
+                </div>
+
+                <div class="row">
+                    
+                </div> 
+
+                <div class="row">
+                    <div id="perfil_Cep"  class="linha_perfil">
+                            <h5>Cep:</h5> <p><?=$linha['cep']?></p>
+                    </div>
+
+                    <div id="perfil_endereco"  class="linha_perfil">
+                        <h5>Endereço:</h5> <p><?=$linha['endereco']?></p>
+                    </div>
+                    <div id="perfil_numero"  class="linha_perfil">
+                        <h5>Numero:</h5> <p><?=$linha['numero']?></p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div id="perfil_Complemento"  class="linha_perfil">
+                        <h5>Complemento:</h5> <p><?=$linha['complemento']?></p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div id="perfil_Cidade"  class="linha_perfil">
+                        <h5>Cidade:</h5> <p><?=$linha['cidade']?></p>
+                    </div>
+
+                    <div id="perfil_Bairro"  class="linha_perfil">
+                        <h5>Bairro:</h5> <p><?=$linha['bairro']?></p>
+                    </div>
+                    
+                </div>
+            
+                <div id="perfil_Estado"></div>
+                
+                
+
+
+
+
+            <?php
+            }
+            ?>
+
+            <div id="perfil_deslogar">
+                <a class="btnPrincipal" href="./script/logoff.php">Deslogar</a>
             </div>
-        
-            <div id="perfil_Estado"></div>
-            
-            
-
-
-
-
-        <?php
-        }
-        ?>
-
-
-        <a href="./script/logoff.php">Deslogar</a>
+        </div>
     </div>
-</div>
+    </div>
 </div>
