754a9b93cbbc62ec11511f3902bf713a4e709bf0
SilvioDiaz <57494579+SilvioDiaz@users.noreply.github.com>
2021-01-11 18:13:27 -0300

    Melhoria no card de produto

--- paginas/home.php
+++ paginas/home.php
@@ -2,69 +2,64 @@
     document.title = "D'marsil Jóias"
 </script>
 
-<div class="containerLista ">
-    <div class='Home' id='produtoHome'>
 
+<div class="container">
+    <div class="containerLista">
+        <div class='Home' id='produtoHome'>
+
+            <?php
+
+            $query = 'SELECT * FROM produto';
+            $consulta = mysqli_query($conexao, $query);
+            while ($linha = mysqli_fetch_array($consulta)) {
+            ?>
+
+            <a class="produto" href="?pagina=pagina_produto&id=<?= $linha['id_produto'] ?>">
+                <div  id="tituloProduto">
+                    <h1><?= $linha['nome_produto'] ?></h1>    
+                </div>
+
+                    <div class="imgProduto" id="imgProduto">
+                        <img class="imgHome" src=<?= $linha['imagem_produto'] ?>>
+                    </div>
+                    <div id="precoProduto">
+                        <h5>R$ <?= $linha['preco_produto'] ?></h5>
+                    </div>
+                
+            </a>
+        
         <?php
 
-        $query = 'SELECT * FROM produto';
-        $consulta = mysqli_query($conexao, $query);
-        while ($linha = mysqli_fetch_array($consulta)) {
-        ?>
-
-        <div class="produto" id="tituloProduto">
-            <h1><?= $linha['nome_produto'] ?></h1>
-
-            <div class="imgProduto" id="imgProduto">
-                <img class="imgHome" src=<?= $linha['imagem_produto'] ?>>
-            </div>
-            <div id="precoProduto">
-                <p>R$ <?= $linha['preco_produto'] ?></p>
-                <div id="btCompra">
-                    <a href="?pagina=pagina_produto&id=<?= $linha['id_produto'] ?>">Comprar</a>
-
-                </div>
-            </div>
+            }
+            ?>
 
         </div>
-    
-    <?php
 
-        }
+        <?php
+                $queryPromo = "SELECT * FROM produto WHERE promocao >0";
+                $consulta = mysqli_query($conexao, $queryPromo);
+                while ($linha = mysqli_fetch_array($consulta)) {
+                            
+            ?>
+
+            <a class="produto" href="?pagina=pagina_produto&id=<?= $linha['id_produto'] ?>">
+                <div  id="tituloProduto">
+                    <h1><?= $linha['nome_produto'] ?></h1>    
+                </div>
+
+                    <div class="imgProduto" id="imgProduto">
+                        <img class="imgHome" src=<?= $linha['imagem_produto'] ?>>
+                    </div>
+                    <div id="precoProduto">
+                        <h5>R$ <?= $linha['preco_produto'] ?></h5>
+                    </div>
+                
+            </a>
+
+            <?php
+                }              
         ?>
+
 
     </div>
-
-    <?php
-            $queryPromo = "SELECT * FROM produto WHERE promocao >0";
-            $consulta = mysqli_query($conexao, $queryPromo);
-            while ($linha = mysqli_fetch_array($consulta)) {
-                           
-        ?>
-
-        <div class="produto" id="tituloProduto">
-            <h1><?= $linha['nome_produto'] ?></h1>
-
-            <div class="imgProduto" id="imgProduto">
-                <img class="imgHome" src=<?= $linha['imagem_produto'] ?>>
-            </div>
-            <div id="precoProduto">
-                <p>R$ <?= $linha['preco_produto'] ?></p>
-                <div id="btCompra">
-                    <a href="?pagina=pagina_produto&id=<?= $linha['id_produto'] ?>">Comprar</a>
-
-                </div>
-            </div>
-
-        </div>
-
-        <?php
-            }
-            PRINT_R($linha);
-                
-       
-
-       ?>
-
-
 </div>
