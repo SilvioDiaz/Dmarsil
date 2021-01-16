7bb9819e5e871c7e89aa4f3812548fa4954632a8
SilvioDiaz <57494579+SilvioDiaz@users.noreply.github.com>
2021-01-15 21:51:18 -0300

    Layout de Login

--- paginas/login.php
+++ paginas/login.php
@@ -13,13 +13,20 @@
 <?php
     }
 ?>
+<?php
+if(isset($_GET['perfil'])=='logar'){
+    echo "<h2 class='tituloPagina'>Faça seu Login</h2>";
+}
+?>
 
-<div class="containerLista ">
-<form class="formulario" action='./script/codigo_login.php?<?=$pagina?>' method='POST'>
-    <input type="text" id="inputLogin"  name="inputLogin" placeholder="Login" required maxlength="10"> <br>
-    <input type="password" id="inputSenha" name="inputSenha" placeholder="Senha" required maxlength="15"> <br>
-    <button class="btnPrincipal" type="submit" id="btnIncluir">Login</button>
-</form>
-<a href = ./index.php?pagina=cadastro>Não tem cadasro? Clique aqui</a>
+<div id="login">
+    <form class="formulario" action='./script/codigo_login.php?<?=$pagina?>' method='POST'>
+        <input class ="inputLogin" type="text" id="inputLogin"  name="inputLogin" placeholder="Login" required maxlength="10"> <br>
+        <input class ="inputLogin" type="password" id="inputSenha" name="inputSenha" placeholder="Senha" required maxlength="15"> <br>
+        <div style = "display: flex; flex-direction: row-reverse">
+            <button class="btnPrincipal" type="submit" id="btnIncluir">Login</button>
+        </div>
+    </form>
+    <a href = ./index.php?pagina=cadastro>Não tem cadasro? Clique aqui</a>
 </div>
 
