7da0a031afce547210e7083266c14660af8d1e73
SilvioDiaz <57494579+SilvioDiaz@users.noreply.github.com>
2021-01-18 20:22:21 -0300

    Footer

--- footer.php
+++ footer.php
@@ -1,1 +1,5 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script></html>
+
+<div id="footer">
+    <h5>Copyright © 2020 Senac Silvio Dias - Todos os direitos reservados.</h5>
+</div>
