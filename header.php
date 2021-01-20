bc4e58f42e369bedc4ea5c39bd1a32727f7f871d
SilvioDiaz <57494579+SilvioDiaz@users.noreply.github.com>
2021-01-20 10:47:44 -0300

    Bootstrap offline

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/reset.css">
     <script src="http://code.jquery.com/jquery-latest.js"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"> 
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" > 
     <link rel="stylesheet" type="text/css" href="css/style.css">
     
 </head>
