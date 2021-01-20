f10c24b15b8a9b44d89d76c693e11139bbab4bac
SilvioDiaz <57494579+SilvioDiaz@users.noreply.github.com>
2021-01-20 00:06:01 -0300

    Pagina Produto terminada

--- paginas/pagina_produto.php
+++ paginas/pagina_produto.php
@@ -12,35 +12,48 @@ $consulta = mysqli_query($conexao, $query);
 
 while($linha = mysqli_fetch_array($consulta)){
 ?>
+<div class="container">
 
+    <div id="areaProduto">
 
-<div class="produto" id="tituloProduto">
-            <h1><?= $linha['nome_produto'] ?></h1>
+        <div  id="produtoDetalhes">
 
-            <div class="imgProduto" id="imgProduto">
-                <img class="imgHome" src=<?= $linha['imagem_produto'] ?>>
-            </div>
-
-            <div class="descricaoProduto">
-            <p><?=$linha['descricao_produto']?></p>
-            </div>
-
-            <div class="detalhesProduto">
-              
-                <p>Tamanho: <?=$linha['tamanho_produto']?></p>
-                <p>Peso: <?=$linha['peso_produto']?></p>
-
-            </div>
-
-            <div id="precoProduto">
-                <p>R$ <?= $linha['preco_produto'] ?></p>
-                <div id="btCompra">
-                    <a href="?pagina=carrinho&id=<?= $linha['id_produto'] ?>&acao=add">Comprar</a>
-
+            <div id="imagem_nome">
+                <div class="col-md-12" id="imgProduto">
+                    <img class="imgHome" src=<?= $linha['imagem_produto'] ?> style="height: 500px;width: 500px;">
                 </div>
+
+                <div class="col-4 col-md-12" id="nome_descricao">
+                    <div id="nome_produto">
+                        <h1><?= $linha['nome_produto'] ?></h1>
+                    </div>
+
+                    <div class="descricaoProduto">
+                        <p><?=$linha['descricao_produto']?></p>
+                    </div>
+
+                    <div class="detalhesProduto">
+                
+                        <p>Tamanho: <?=$linha['tamanho_produto']?>cm</p>
+                        <p>Peso: <?=$linha['peso_produto']?>g</p>
+
+                    </div>
+                </div>
+
+                
+                <div class="col-4" id="precoProduto">
+                 
+                    <p id="precoArea">R$ <?= $linha['preco_produto'] ?></p>
+                    <a style="width: 50%; text-align: center;" class="btnPrincipal" href="?pagina=carrinho&id=<?= $linha['id_produto'] ?>&acao=add">Comprar</a>
+
+                
+                 </div>
+
             </div>
 
         </div>
+    </div>
+</div>
 
 
 <?php
